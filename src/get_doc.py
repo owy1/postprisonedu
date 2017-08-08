966761767767253e2b20e69955e253e8a0ea674d
Joe Sirott <spammy@sirvin.com>
2017-08-08 16:27:02 -0700

    Handle names as well as docid Return multiple values (for name search) Fix ASP state info problems

--- src/get_doc.py
+++ src/get_doc.py
@@ -3,28 +3,38 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import argparse
+import urllib
 
 # DOC Web site
-_BaseUrl = 'http://www.doc.wa.gov/information/inmate-search/default.aspx'
+_BaseUrl='http://www.doc.wa.gov/information/inmate-search/default.aspx'
 
 # Ugly set of POST arguments required by DOC Web site
-_PostArgs = '_LASTFOCUS=&__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE=%2FwEPDwULLTExODIwNjM0ODhkZKzRISt7X2wgEn2vPGJSCpDazTB%2F&__VIEWSTATEGENERATOR=49D7FAF8&__EVENTVALIDATION=%2FwEWAwLZm5CKDQLs0bLrBgKM54rGBtTJonjNDNMNp6w59DBLP%2F8kvVPI&Button1=Submit'
+_PostArgs='__VIEWSTATE=%s&__VIEWSTATEGENERATOR=%s&__EVENTVALIDATION=%s&Button1=Submit&TextBox1=%s'
 
 def get_doc(docid):
-    """
+    '''
     Request information from DOC Web site based on inmate's DOC number
-    :param docid:
+    :param docid: 
     :return: JSON string with inmate info (or empty JSON if no match)
-    """
-    payload = _PostArgs + '&TextBox1=%d' % docid
-    r = requests.post(_BaseUrl, data=payload, headers={'Content-type': 'application/x-www-form-urlencoded'})
-    html = BeautifulSoup(r.text, 'html.parser')
-    tds = [td.text.strip().replace(':', '') for td in html.find_all('td')]
-    return dict(zip(tds[::2], tds[1::2]))
+    '''
+    # First request to get ASP.net state info
+    r = requests.get(_BaseUrl)
+    soup = BeautifulSoup(r.text,'html.parser')
+    viewstate = urllib.quote(soup.select("#__VIEWSTATE")[0]['value'],safe='')
+    eventvalidation = urllib.quote(soup.select("#__EVENTVALIDATION")[0]['value'],safe='')
+    vsg = urllib.quote(soup.select("#__VIEWSTATEGENERATOR")[0]['value'],safe='')
+    payload = _PostArgs % (viewstate,vsg,eventvalidation,docid)
+
+    # Now post and retrieve results
+    r = requests.post(_BaseUrl,data=payload,headers={'Content-type':'application/x-www-form-urlencoded'})
+    soup = BeautifulSoup(r.text,'html.parser')
+    tds = [td.text.strip().replace(':','') for td in soup.find_all('td')]
+    rval = [dict(zip(tds[i:i+8:2],tds[i+1:i+9:2])) for i in range(len(tds)) if i%8 == 0]
+    return rval
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Get DOC info on an inmate')
-    parser.add_argument('docid', type=int, nargs=1, help='DOC id (for example, 118603)')
+    parser.add_argument('docid', type=str, nargs=1, help='DOC id or name(for example, 118603)')
     FLAGS, unparsed = parser.parse_known_args()
     print(json.dumps(get_doc(FLAGS.docid[0])))
