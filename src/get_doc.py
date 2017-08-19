5feeb17e9bcb9517218124ed2d83831ca3add5d5
Joe Sirott <spammy@sirvin.com>
2017-08-19 15:43:06 -0700

    Major changes: 1) Allow query of SalesForce contact database by lastname. Also limit number of returned values 2) Update all SalesForce records that have a DOC id with the DOC institution location, if available

--- src/get_doc.py
+++ src/get_doc.py
@@ -7,9 +7,8 @@ import argparse
 import urllib
 from six.moves.urllib.parse import quote
 from simple_salesforce import Salesforce
-import pytz
-import datetime
 import json
+import logging as log
 
 # DOC Web site
 _BaseUrl='http://www.doc.wa.gov/information/inmate-search/default.aspx'
@@ -17,36 +16,16 @@ _BaseUrl='http://www.doc.wa.gov/information/inmate-search/default.aspx'
 # Ugly set of POST arguments required by DOC Web site
 _PostArgs='__VIEWSTATE=%s&__VIEWSTATEGENERATOR=%s&__EVENTVALIDATION=%s&Button1=Submit&TextBox1=%s'
 
-
 def get_login():
-    """."""
+    """
+    Get a handle to a SalesForce object after authentication
+    :return: SalesForce object
+    """
     username = os.environ.get('username')
     password = os.environ.get('password')
     security_token = os.environ.get('security_token')
     sf = Salesforce(username=username, password=password, security_token=security_token)
     return sf
-
-def get_doc(docid):
-    '''
-    Request information from DOC Web site based on inmate's DOC number
-    :param docid:
-    :return: JSON string with inmate info (or empty JSON if no match)
-    '''
-    # First request to get ASP.net state info
-    r = requests.get(_BaseUrl)
-    soup = BeautifulSoup(r.text,'html.parser')
-    viewstate = quote(soup.select("#__VIEWSTATE")[0]['value'],safe='')
-    eventvalidation = quote(soup.select("#__EVENTVALIDATION")[0]['value'],safe='')
-    vsg = quote(soup.select("#__VIEWSTATEGENERATOR")[0]['value'],safe='')
-    payload = _PostArgs % (viewstate,vsg,eventvalidation,docid)
-
-    # Now post and retrieve results
-    r = requests.post(_BaseUrl,data=payload,headers={'Content-type':'application/x-www-form-urlencoded'})
-    soup = BeautifulSoup(r.text,'html.parser')
-    tds = [td.text.strip().replace(':','') for td in soup.find_all('td')]
-    rval = [dict(zip(tds[i:i+8:2],tds[i+1:i+9:2])) for i in range(len(tds)) if i%8 == 0]
-    return rval
-
 def debug(o,indent=4):
     '''
     Pretty print SalesForce objects
@@ -55,21 +34,61 @@ def debug(o,indent=4):
     '''
     print(json.dumps(o,indent=indent))
 
+def get_doc_info(sfrecords):
+    '''
+    Request information from DOC Web site based on inmate's DOC number obtained from SalesForce.
+    and add to SalesForce record. New fields are DOCLocation (which institution the inmate is 
+    incarcerated in and DOCName, the name of the inmate as registered by the DOC
+    :param sfrecords:
+    :return: JSON string with inmate info (or empty JSON if no match)
+    '''
+    # First request to get ASP.net state info
+    r = requests.get(_BaseUrl)
+    soup = BeautifulSoup(r.text,'html.parser')
+    viewstate = quote(soup.select("#__VIEWSTATE")[0]['value'],safe='')
+    eventvalidation = quote(soup.select("#__EVENTVALIDATION")[0]['value'],safe='')
+    vsg = quote(soup.select("#__VIEWSTATEGENERATOR")[0]['value'],safe='')
 
-def get_sf_ids():
-    #debug(sf.Contact.describe())
-    #raise Exception
+    # Now post and retrieve results
+    info = []
+    for record in sfrecords:
+        docid = record['CorrectionsAgencyNum__c']
+        record['DOCLocation'] = None
+        if docid is None:
+            log.warn("No DOC id available for %s" % record.get('Name'))
+            continue
+        payload = _PostArgs % (viewstate, vsg, eventvalidation, docid)
+        r = requests.post(_BaseUrl,data=payload,headers={'Content-type':'application/x-www-form-urlencoded'})
+        soup = BeautifulSoup(r.text,'html.parser')
+        tds = [td.text.strip().replace(':','') for td in soup.find_all('td')]
+        docinfo = dict(zip(tds[:8:2],tds[1:9:2]))
+        record['DOCLocation'] = docinfo.get('Location')
+        record['DOCName'] = docinfo.get('Offender Name')
+        info.append(record)
+    return info
+
+def get_sf_ids(lastname=None, limit=None):
+    """
+    Get contact info from PostPrison db.
+    :param lastname: Lastname of contact in SF db. If None, all lastnames
+    :param limit: Maximum number of objects to return. If None, no limit
+    :return: List of SF objects matching query
+    """
+    sqlquery = "SELECT Id,LastName,FirstName,Name,CorrectionsAgencyNum__c,LastActivityDate from Contact "
+    if lastname is not None:
+        sqlquery += " where LastName='%s' " % lastname
+    if limit is not None:
+        sqlquery += " limit %d" % limit
+
     docids = []
-    for record in sf.query_all("SELECT LastName,FirstName,Name,CorrectionsAgencyNum__c from Contact where LastName='Jones' limit 1")['records']:
-        debug(record)
-        docids.append(int(record['CorrectionsAgencyNum__c']))
+    for record in sf.query_all(sqlquery)['records']:
+        docid = record.get('CorrectionsAgencyNum__c')
+        if docid is not None:
+            record['CorrectionsAgencyNum__c'] = int(docid)
+        del record['attributes']
+        docids.append(record)
     return docids
 
 if __name__ == '__main__':
     sf = get_login()
-    temp = get_sf_ids()
-    debug(get_doc(temp[0]))
-   # parser = argparse.ArgumentParser(description='Get DOC info on an inmate')
-   # parser.add_argument('docid', type=str, nargs=1, help='DOC id or name(for example, 118603)')
-   # FLAGS, unparsed = parser.parse_known_args()
-   # print(json.dumps(get_doc(FLAGS.docid[0])))
+    debug(get_doc_info(get_sf_ids(lastname='Jones', limit=5)))
