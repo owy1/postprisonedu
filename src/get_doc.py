079dac7f3357ff7f0029d37338b0e95097449729
Joe Sirott <spammy@sirvin.com>
2017-08-24 15:50:57 -0700

    checkpoint

--- src/get_doc.py
+++ src/get_doc.py
@@ -16,16 +16,83 @@ _BaseUrl='http://www.doc.wa.gov/information/inmate-search/default.aspx'
 # Ugly set of POST arguments required by DOC Web site
 _PostArgs='__VIEWSTATE=%s&__VIEWSTATEGENERATOR=%s&__EVENTVALIDATION=%s&Button1=Submit&TextBox1=%s'
 
-def get_login():
-    """
-    Get a handle to a SalesForce object after authentication
-    :return: SalesForce object
-    """
-    username = os.environ.get('username')
-    password = os.environ.get('password')
-    security_token = os.environ.get('security_token')
-    sf = Salesforce(username=username, password=password, security_token=security_token)
-    return sf
+class PostPrisonSF(object):
+    def __init__(self,username=None, password=None, security_token=None):
+        self.sf = Salesforce(username=username, password=password, security_token=security_token)
+
+    def query(self,lastname=None, limit=None):
+        """
+        Get contact info from PostPrison db.
+        :param lastname: Lastname of contact in SF db. If None, all lastnames
+        :param limit: Maximum number of objects to return. If None, no limit
+        :return: List of SF objects matching query
+        """
+        sf = self.sf
+        sqlquery = "SELECT Id,LastName,FirstName,Name,CorrectionsAgencyNum__c,DOCAgencyNumType__c,LastActivityDate from Contact "
+        if lastname is not None:
+            sqlquery += " where LastName='%s' " % lastname
+        if limit is not None:
+            sqlquery += " limit %d" % limit
+
+        records = []
+        unsupportedDocTypes = set()
+        bad = 0
+        for record in sf.query_all(sqlquery)['records']:
+            # debug(record)
+            docid = record.get('CorrectionsAgencyNum__c')
+            if docid is not None:
+                try:
+                    agencyNum = record['DOCAgencyNumType__c']
+                    if agencyNum is None or not agencyNum.startswith('WA DOC'):
+                        if agencyNum is not None:
+                            bad += 1
+                            unsupportedDocTypes.add(record['DOCAgencyNumType__c'])
+                        raise ValueError("Not a DOC id")
+                    record['CorrectionsAgencyNum__c'] = int(docid)
+                    del record['attributes']
+                    records.append(record)
+                except ValueError as e:
+                    log.debug("Error reading id for %s: %s" % (record.get('Name'), e.message))
+                    log.debug("Bad record was %s" % json.dumps(record, indent=4))
+
+        log.debug("Unsupported doc types=%s" % unsupportedDocTypes)
+        log.info("Fail/good %d/%d" % (bad, len(records)))
+        return self._get_doc_info(records)
+
+    def _get_doc_info(self, sfrecords):
+        '''
+        Request information from DOC Web site based on inmate's DOC number obtained from SalesForce
+        and add to SalesForce record. New fields are DOCLocation (which institution the inmate is 
+        incarcerated in and DOCName, the name of the inmate as registered by the DOC
+        :param sfrecords:
+        :return: JSON string with inmate info (or empty JSON if no match)
+        '''
+        # First request to get ASP.net state info
+        session = requests.Session()
+        r = session.get(_BaseUrl)
+        soup = BeautifulSoup(r.text,'html.parser')
+        viewstate = quote(soup.select("#__VIEWSTATE")[0]['value'],safe='')
+        eventvalidation = quote(soup.select("#__EVENTVALIDATION")[0]['value'],safe='')
+        vsg = quote(soup.select("#__VIEWSTATEGENERATOR")[0]['value'],safe='')
+
+        # Now post and retrieve results
+        info = []
+        for record in sfrecords:
+            docid = record['CorrectionsAgencyNum__c']
+            record['DOCLocation'] = None
+            if docid is None:
+                log.warn("No DOC id available for %s in SalesForce Contact info" % record.get('Name'))
+                continue
+            payload = _PostArgs % (viewstate, vsg, eventvalidation, docid)
+            r = session.post(_BaseUrl,data=payload,headers={'Content-type':'application/x-www-form-urlencoded'})
+            soup = BeautifulSoup(r.text,'html.parser')
+            tds = [td.text.strip().replace(':','') for td in soup.find_all('td')]
+            docinfo = dict(zip(tds[:8:2],tds[1:9:2]))
+            record['DOCLocation'] = docinfo.get('Location')
+            record['DOCName'] = docinfo.get('Offender Name')
+            info.append(record)
+        return info
+
 
 def debug(o,indent=4):
     '''
@@ -35,85 +102,19 @@ def debug(o,indent=4):
     '''
     print(json.dumps(o,indent=indent))
 
-def get_doc_info(sfrecords):
-    '''
-    Request information from DOC Web site based on inmate's DOC number obtained from SalesForce
-    and add to SalesForce record. New fields are DOCLocation (which institution the inmate is 
-    incarcerated in and DOCName, the name of the inmate as registered by the DOC
-    :param sfrecords:
-    :return: JSON string with inmate info (or empty JSON if no match)
-    '''
-    # First request to get ASP.net state info
-    session = requests.Session()
-    r = session.get(_BaseUrl)
-    soup = BeautifulSoup(r.text,'html.parser')
-    viewstate = quote(soup.select("#__VIEWSTATE")[0]['value'],safe='')
-    eventvalidation = quote(soup.select("#__EVENTVALIDATION")[0]['value'],safe='')
-    vsg = quote(soup.select("#__VIEWSTATEGENERATOR")[0]['value'],safe='')
 
-    # Now post and retrieve results
-    info = []
-    for record in sfrecords:
-        docid = record['CorrectionsAgencyNum__c']
-        record['DOCLocation'] = None
-        if docid is None:
-            log.warn("No DOC id available for %s in SalesForce Contact info" % record.get('Name'))
-            continue
-        payload = _PostArgs % (viewstate, vsg, eventvalidation, docid)
-        r = session.post(_BaseUrl,data=payload,headers={'Content-type':'application/x-www-form-urlencoded'})
-        soup = BeautifulSoup(r.text,'html.parser')
-        tds = [td.text.strip().replace(':','') for td in soup.find_all('td')]
-        docinfo = dict(zip(tds[:8:2],tds[1:9:2]))
-        record['DOCLocation'] = docinfo.get('Location')
-        record['DOCName'] = docinfo.get('Offender Name')
-        info.append(record)
-    return info
+# def get_sf_by_id(id):
+#     return sf.Contact.get(id)
 
-def get_sf_by_id(id):
-    return sf.Contact.get(id)
-
-def get_sf_ids(lastname=None, limit=None):
-    """
-    Get contact info from PostPrison db.
-    :param lastname: Lastname of contact in SF db. If None, all lastnames
-    :param limit: Maximum number of objects to return. If None, no limit
-    :return: List of SF objects matching query
-    """
-    sqlquery = "SELECT Id,LastName,FirstName,Name,CorrectionsAgencyNum__c,DOCAgencyNumType__c,LastActivityDate from Contact "
-    if lastname is not None:
-        sqlquery += " where LastName='%s' " % lastname
-    if limit is not None:
-        sqlquery += " limit %d" % limit
-
-    docids = []
-    unsupportedDocTypes = set()
-    bad = 0
-    for record in sf.query_all(sqlquery)['records']:
-        #debug(record)
-        docid = record.get('CorrectionsAgencyNum__c')
-        if docid is not None:
-            try:
-                agencyNum = record['DOCAgencyNumType__c']
-                if agencyNum is None or not agencyNum.startswith('WA DOC'):
-                    if agencyNum is not None:
-                        bad += 1
-                        unsupportedDocTypes.add(record['DOCAgencyNumType__c'])
-                    raise ValueError("Not a DOC id")
-                record['CorrectionsAgencyNum__c'] = int(docid)
-                del record['attributes']
-                docids.append(record)
-            except ValueError as e:
-                log.debug("Error reading id for %s: %s" % (record.get('Name'),e.message))
-                log.debug("Bad record was %s" % json.dumps(record,indent=4))
-
-    log.debug("Unsupported doc types=%s" % unsupportedDocTypes)
-    log.info("Fail/good %d/%d" % (bad,len(docids)))
-    return docids
 
 if __name__ == '__main__':
-    sf = get_login()
+    username = os.environ.get('username')
+    password = os.environ.get('password')
+    security_token = os.environ.get('security_token')
+    pp = PostPrisonSF(username=username, password=password, security_token=security_token)
+    debug(pp.query(lastname='Jones',limit=10))
     #get_sf_ids()
-    debug(get_doc_info(get_sf_ids(limit=100)))
+    #debug(get_doc_info(get_sf_ids(limit=500)))
     #debug(get_doc_info(get_sf_ids(lastname='Jones',limit=10)))
     #debug(get_doc_info(get_sf_ids()))
 
@@ -126,11 +127,16 @@ if __name__ == '__main__':
 
     # d = get_doc_info(get_sf_ids(lastname='Chertok'))
     # debug(d)
-    #d = get_sf_by_id('003i000000gDakgAAC')
-
+    # d = get_sf_by_id('003i000000gDakgAAC')
+    #
     # for k in sorted(d.keys()):
     #     v = d[k]
-    #     if v is not None:
+    #     if v is not None and not k.endswith('__c'):
     #         print(k,':',v)
-
+    # print()
+    # for k in sorted(d.keys()):
+    #     v = d[k]
+    #     if v is not None and k.endswith('__c'):
+    #         print(k,':',v)
+    #
 
